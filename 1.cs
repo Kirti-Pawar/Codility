87f0b6ecb4ee982e5a830d94098b48b903cb5ca6
Kirti Pawar <kirti.pawar@lntinfotech.com>
2020-12-16 09:54:35 +0530

    Updated

--- 1.cs
+++ 1.cs
@@ -6,13 +6,13 @@ class D1
 static void Main()
 {
 	int[] A=new int[]{1,4,1}; 
-	int res =0;
+	int res1 =0;
 	for (int i = 0; i < A.Length; i++)  
       	{ 
-           		res = res ^ A[i]; 
-		Console.Write(" "+res);
+           		res1 = res1^ A[i]; 
+		Console.Write(" "+res1);
         	} 
-Console.WriteLine("\n"+res);
+Console.WriteLine("\n"+res1);
 }
 }
 }
